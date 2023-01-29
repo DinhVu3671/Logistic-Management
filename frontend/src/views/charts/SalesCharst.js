e8d35a5324a9a48e848246dfdf55bd8f73e46fb3
buiviethoang <buiviethoang12062000@gmail.com>
2023-01-29 09:23:26 +0700

    change UI and apis

--- frontend/src/views/charts/SalesCharst.js
+++ frontend/src/views/charts/SalesCharst.js
@@ -1,111 +1,91 @@
-import {
-  CCard,
-  CCardBody,
-  CCardHeader,
-  CCol,
-  CDataTable,
-  CRow,
-  CPagination,
-  CInputGroup,
-  CInputGroupPrepend,
-  CInputGroupText,
-  CInput,
-  CButton,
-  CInputGroupAppend,
-  CContainer,
-  CButtonGroup,
-  CToastBody,
-  CToastHeader,
-  CToast,
-  CToaster,
-  CModalHeader,
-  CModalTitle,
-  CModal,
-  CModalBody,
-  CFormGroup,
-  CLabel,
-  CModalFooter,
-  CCardText,
-} from '@coreui/react'
-import CIcon from '@coreui/icons-react'
+import { CContainer, CRow } from "@coreui/react";
 
-import React, { PureComponent, useEffect, useState } from 'react';
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-import DashboardService from '../../service/DashboardService';
+import React, { useEffect, useState } from "react";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import DashboardService from "../../service/DashboardService";
 
 const dataSales = [
   {
-    name: 'Tháng 1',
+    name: "Tháng 1",
     Doanh_thu: 0,
   },
   {
-    name: 'Tháng 2',
+    name: "Tháng 2",
     Doanh_thu: 0,
   },
   {
-    name: 'Tháng 3',
+    name: "Tháng 3",
     Doanh_thu: 0,
   },
   {
-    name: 'Tháng 4',
+    name: "Tháng 4",
     Doanh_thu: 0,
   },
   {
-    name: 'Tháng 5',
+    name: "Tháng 5",
     Doanh_thu: 0,
   },
   {
-    name: 'Tháng 6',
+    name: "Tháng 6",
     Doanh_thu: 0,
   },
   {
-    name: 'Tháng 7',
+    name: "Tháng 7",
     Doanh_thu: 0,
   },
   {
-    name: 'Tháng 8',
+    name: "Tháng 8",
     Doanh_thu: 0,
   },
   {
-    name: 'Tháng 9',
+    name: "Tháng 9",
     Doanh_thu: 0,
   },
   {
-    name: 'Tháng 10',
+    name: "Tháng 10",
     Doanh_thu: 0,
   },
   {
-    name: 'Tháng 11',
+    name: "Tháng 11",
     Doanh_thu: 0,
   },
   {
-    name: 'Tháng 12',
+    name: "Tháng 12",
     Doanh_thu: 0,
   },
 ];
 
 const SalesCharst = () => {
-  const [statInterval, setStatInterval] = useState("2022");
-  const [dataFinal, setDataFinal] = useState([])
+  const [statInterval, setStatInterval] = useState("2023");
+  const [dataFinal, setDataFinal] = useState([]);
 
   useEffect(() => {
-    DashboardService.sales(statInterval).then(response => {
+    DashboardService.sales(statInterval).then((response) => {
       if (response.data.code === "SUCCESS") {
         var data = response.data.data;
         data.map((item) => {
           let month = new Date(item[1]).getMonth();
-          dataSales[month - 1].Doanh_thu = dataSales[month - 1].Doanh_thu + item[0]
-        })
-        setDataFinal(dataSales)
+          dataSales[month - 1].Doanh_thu =
+            dataSales[month - 1].Doanh_thu + item[0];
+        });
+        setDataFinal(dataSales);
       }
     });
   }, []);
   return (
     <>
       <CContainer>
-        <CRow style={{display: "flex", justifyContent: "center"}}>
+        <CRow style={{ display: "flex", justifyContent: "center" }}>
           <div style={{ marginTop: "20px", fontWeight: 700, fontSize: 20 }}>
-            DOANH THU TỔNG HỢP 2022
+            DOANH THU TỔNG HỢP 2023
           </div>
           <div style={{ marginTop: "20px", backgroundColor: "white" }}>
             <BarChart
@@ -128,12 +108,10 @@ const SalesCharst = () => {
               {/* <Bar dataKey="uv" fill="#82ca9d" /> */}
             </BarChart>
           </div>
-
         </CRow>
-
       </CContainer>
     </>
-  )
-}
+  );
+};
 
-export default SalesCharst
+export default SalesCharst;
